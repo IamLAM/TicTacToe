170d23d9623c74aac1f73950b4b67f6fe64b3d0d
IamLAM <ingeniero@luismonge.com.mx>
2019-04-15 21:55:14 -0700

    Using method board and square render

--- tictac/src/index.js
+++ tictac/src/index.js
@@ -5,13 +5,13 @@ import './index.css';
 
 class Square extends React.Component {
     render() {
-        return ( < button className = "square" > { /* TODO */ } < /button>);
+        return ( < button className = "square" > { this.props.value} < /button>);
     }
 }
 
 class Board extends React.Component {
     renderSquare(i) {
-        return <Square / > ;
+        return <Square value={i} /> ;
     }
 
     render() {
